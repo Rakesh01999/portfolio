544181afd12574f8f7fb4b87850628cda84c04ba
Rakesh Biswas <rbiswas01999@gmail.com>
2024-05-13 14:39:14 +0600

    services added

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Fade, Zoom } from 'react-awesome-reveal';
+import { BiSolidCalendarEvent } from 'react-icons/bi';
 import { FaHotel, FaPhone } from 'react-icons/fa6';
 import { FcBusinessman } from "react-icons/fc";
+import { IoMan } from 'react-icons/io5';
+import { MdOutlineRateReview } from 'react-icons/md';
+import { RiPhoneFindFill } from 'react-icons/ri';
 import { SiNginxproxymanager } from 'react-icons/si';
 import { TbWorld } from 'react-icons/tb';
+import { TfiHeadphoneAlt } from 'react-icons/tfi';
 import { Typewriter } from 'react-simple-typewriter';
 
 
@@ -12,8 +17,8 @@ const Services = () => {
         <div>
 
             <Fade>
-                <h2 className="hover:text-lime-500 text-lg text-center md:text-[40px] text-blue-500 font-bold mb-6"> Services </h2>
-                <h2 className="text-xl text-center md:text-[30px] text-lime-700 font-bold mb-10">
+                <h2 className="hover:text-cyan-300 text-lg text-center md:text-[40px] text-blue-500 font-bold mb-6"> Services </h2>
+                <h2 className="text-xl text-center md:text-[30px] text-cyan-400 font-bold mb-10">
                     <Typewriter
                         loop
                         cursor
@@ -23,7 +28,7 @@ const Services = () => {
                         delaySpeed={1000}
                         // words={['Hello', 'World', 'React']}
                         // words={[' your Desired country cards', ' to find best tourists places']}
-                        words={[' Tours & ', ' Travel Services']}
+                        words={[' Book  Buddy Services']}
                     />
                 </h2>
             </Fade>
@@ -31,36 +36,34 @@ const Services = () => {
             <div className='max-w-[300px] md:max-w-[500px] lg:max-w-[1540px] mx-auto px-4 md:px-8 py-8 md:py-12  rounded-3xl flex flex-col md:flex-row items-center mb-10'>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
+
                     <Zoom>
                         {/* ------------- card 1 -------------- */}
                         <div className="hover:shadow-2xl hover:rounded-3xl text-center mb-2">
                             {/* <button className="btn btn-outline btn-info md:w-[200px] rounded-full md:text-lg text-white mb-4"> */}
                             <div className="hover:shadow-2xl hover:rounded-3xl">
-                                <div className="max-w-full md:h-[700px] bg-blue-100 rounded-3xl shadow-2xl flex flex-col items-center mb-10">
+                                <div className="max-w-full md:h-[700px] bg-cyan-100 rounded-3xl shadow-2xl flex flex-col items-center mb-10">
                                     <figure className="md:px-10 mt-10">
                                         {/* <img src="" alt="house" className="  rounded-xl" /> */}
-                                        <FcBusinessman className='text-9xl' />
+                                        <RiPhoneFindFill className='text-4xl md:text-6xl lg:text-9xl text-blue-600' />
                                     </figure>
                                     <div className="card-body">
                                         {/* <h2 className="card-title text-xl lg:text-2xl text-blue-500 mx-auto font-bold my-4">{spotName}</h2> */}
-                                        <div className="flex gap-2 items-center text-xl text-green-700 md:text-2xl">
+                                        <div className="flex gap-2 items-center text-xl text-blue-600 md:text-2xl">
                                             {/* <IoPricetags className="text-green-200" /> */}
                                             {/* <FaFlag className="text-center" /> */}
-                                            <p className='text-center font-bold'>Travel Guides</p>
+                                            <p className='text-center font-bold'>Book Catagory and Findings</p>
                                         </div>
                                         <div className="border-b-2 my-4"></div>
                                         <div className="flex flex-col gap-4">
                                             <div className="flex gap-2 items-center text-lg md:text-xl">
                                                 {/* <MdDescription className='text-[80px]' /> */}
                                                 <p>
-                                                    Our Travel Guides for expert insights on global destinations. From iconic landmarks to hidden gems, plan your next adventure with our curated recommendations and insider tips.
+                                                    Allow users to find books other resources in the online library’s collection.
+                                                    Also Provide detailed information about each item, including availability status.
                                                 </p>
                                             </div>
-                                            <div className='flex items-center mx-auto mt-4'>
-                                                <p className='flex text-xl gap-6 items-center'>
-                                                    <FaPhone className='text-3xl text-lime-600' />
-                                                    880 1979180341
-                                                </p>
+                                            <div className='flex items-start'>
                                             </div>
                                         </div>
                                     </div>
@@ -76,27 +79,31 @@ const Services = () => {
                         <div className="hover:shadow-2xl hover:rounded-3xl text-center mb-2">
                             {/* <button className="btn btn-outline btn-info md:w-[200px] rounded-full md:text-lg text-white mb-4"> */}
                             <div className="hover:shadow-2xl hover:rounded-3xl">
-                                <div className="max-w-full md:h-[700px] bg-blue-100 rounded-3xl shadow-2xl flex flex-col items-center mb-10">
+                                <div className="max-w-full md:h-[700px] bg-cyan-100 rounded-3xl shadow-2xl flex flex-col items-center mb-10">
                                     <figure className="md:px-10 mt-10">
                                         {/* <img src="" alt="house" className="  rounded-xl" /> */}
-                                        <FaHotel className='text-9xl text-lime-600' />
+                                        <TfiHeadphoneAlt className='text-4xl md:text-6xl lg:text-9xl text-blue-600' />
                                     </figure>
                                     <div className="card-body">
                                         {/* <h2 className="card-title text-xl lg:text-2xl text-blue-500 mx-auto font-bold my-4">{spotName}</h2> */}
-                                        <div className="flex gap-2 items-center text-xl text-green-700 md:text-2xl">
+                                        <div className="flex gap-2 items-center text-xl text-blue-600 md:text-2xl">
                                             {/* <IoPricetags className="text-green-200" /> */}
                                             {/* <FaFlag className="text-center" /> */}
-                                            <p className='text-center font-bold'>Hotel Booking</p>
+                                            <p className='text-center font-bold'>Guides</p>
                                         </div>
                                         <div className="border-b-2 my-4"></div>
                                         <div className="flex flex-col gap-4">
                                             <div className="flex gap-2 items-center text-lg md:text-xl">
                                                 {/* <MdDescription className='text-[80px]' /> */}
                                                 <p>
-                                                    Find your perfect accommodation with ease using our hotel booking platform. Discover a variety of options tailored to your preferences.
+                                                    Meet our Librarian for any serious issue. Feel free to ask any questions . Also you are requested to give us your feedback .
                                                 </p>
                                             </div>
-                                            <div className='flex items-start'>
+                                            <div className='flex items-center mx-auto mt-4'>
+                                                <p className='flex text-xl gap-6 items-center'>
+                                                    <FaPhone className='text-3xl text-blue-500' />
+                                                    880 1879182341
+                                                </p>
                                             </div>
                                         </div>
                                     </div>
@@ -112,14 +119,14 @@ const Services = () => {
                         <div className="hover:shadow-2xl hover:rounded-3xl text-center mb-2">
                             {/* <button className="btn btn-outline btn-info md:w-[200px] rounded-full md:text-lg text-white mb-4"> */}
                             <div className="hover:shadow-2xl hover:rounded-3xl">
-                                <div className="max-w-full md:h-[700px] bg-blue-100 rounded-3xl shadow-2xl flex flex-col items-center mb-10">
+                                <div className="max-w-full md:h-[700px] bg-cyan-100 rounded-3xl shadow-2xl flex flex-col items-center mb-10">
                                     <figure className="md:px-10 mt-10">
                                         {/* <img src="" alt="house" className="  rounded-xl" /> */}
-                                        <SiNginxproxymanager className='text-9xl text-lime-600' />
+                                        <BiSolidCalendarEvent className='text-4xl md:text-6xl lg:text-9xl text-blue-600' />
                                     </figure>
                                     <div className="card-body">
                                         {/* <h2 className="card-title text-xl lg:text-2xl text-blue-500 mx-auto font-bold my-4">{spotName}</h2> */}
-                                        <div className="flex gap-2 items-center text-xl text-green-700 md:text-2xl">
+                                        <div className="flex gap-2 items-center text-xl text-blue-600 md:text-2xl">
                                             {/* <IoPricetags className="text-green-200" /> */}
                                             {/* <FaFlag className="text-center" /> */}
                                             <p className='text-center font-bold'>Event Management</p>
@@ -129,7 +136,8 @@ const Services = () => {
                                             <div className="flex gap-2 items-center text-lg md:text-xl">
                                                 {/* <MdDescription className='text-[80px]' /> */}
                                                 <p>
-                                                    Let us handle the details of your next event with our professional event management services. We ensure seamless planning and execution for a memorable event.
+                                                    Promote library events, workshops, and programs.
+                                                    Maintain an events calendar for users to stay informed.
                                                 </p>
                                             </div>
                                             <div className='flex items-start'>
@@ -148,24 +156,24 @@ const Services = () => {
                         <div className="hover:shadow-2xl hover:rounded-3xl text-center mb-2">
                             {/* <button className="btn btn-outline btn-info md:w-[200px] rounded-full md:text-lg text-white mb-4"> */}
                             <div className="hover:shadow-2xl hover:rounded-3xl">
-                                <div className="max-w-full md:h-[700px] bg-blue-100 rounded-3xl shadow-2xl flex flex-col items-center mb-10">
+                                <div className="max-w-full md:h-[700px] bg-cyan-100 rounded-3xl shadow-2xl flex flex-col items-center mb-10">
                                     <figure className="md:px-10 mt-10">
                                         {/* <img src="" alt="house" className="  rounded-xl" /> */}
-                                        <TbWorld className='text-9xl text-lime-600' />
+                                        <MdOutlineRateReview className='text-4xl md:text-6xl lg:text-9xl text-blue-600' />
                                     </figure>
                                     <div className="card-body">
                                         {/* <h2 className="card-title text-xl lg:text-2xl text-blue-500 mx-auto font-bold my-4">{spotName}</h2> */}
-                                        <div className="flex gap-2 items-center text-xl text-green-700 md:text-2xl">
+                                        <div className="flex gap-2 items-center text-xl text-blue-600 md:text-2xl">
                                             {/* <IoPricetags className="text-green-200" /> */}
                                             {/* <FaFlag className="text-center" /> */}
-                                            <p className='text-center font-bold'>Worldwide Tours</p>
+                                            <p className='text-center font-bold'>Book Recommendations and Reviews</p>
                                         </div>
                                         <div className="border-b-2 my-4"></div>
                                         <div className="flex flex-col gap-4">
                                             <div className="flex gap-2 items-center text-lg md:text-xl">
                                                 {/* <MdDescription className='text-[80px]' /> */}
                                                 <p>
-                                                    Embark on unforgettable journeys around the globe with our curated worldwide tours. Immerse yourself in diverse cultures and enriching experiences .
+                                                    Enable users to borrow books online by checking availability, reserving books that are currently checked out, and scheduling pick-up or delivery of reserved items.
                                                 </p>
                                             </div>
                                             <div className='flex items-start'>
